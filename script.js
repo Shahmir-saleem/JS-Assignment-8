5c0c7fee0c7265db27d952ccd7e962180073c40a
Shahmir-saleem <157698728+Shahmir-saleem@users.noreply.github.com>
2024-06-01 15:08:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+    // task 1 
+// var number  = prompt('Enter a positive integers ');
+// document.write("Number :" + number + '<br>'); 
+// document.write('Round of value: ' + Math.round(number) + '<br>')
+// document.write('Floor value: ' + Math.floor(number) + '<br>')
+// document.write('Ceil value: ' + Math.ceil(number) + '<br>')
+
+// // task 2  
+// var number  = prompt('Enter a negative floatting point number ');
+// document.write("Number :" + number + '<br>'); 
+//  document.write('Round of value: ' + Math.round(number) + '<br>')
+//  document.write('Floor value: ' + Math.floor(number) + '<br>')
+//  document.write('Ceil value: ' + Math.ceil(number) + '<br>')
+
+// task 3 
+// var number  = prompt('Enter a number')
+// var absoluteValue = Math.abs(number);
+// document.write('The absolute value of' + number + 'is' + absoluteValue)
+
+// task 4 
+
+// var value = Math.floor(Math.random()*6) + 1;
+// document.write('The value of dice is ' + value)
+
+// task 5 
+
+// var coinValue = Math.random() < 0.5 ? 'Heads' : 'Tails'
+// document.write('The value of coin is ' + coinValue)
+
+// task 6 
+
+// var randomNumber = Math.floor(Math.random() * 100) + 1;
+// document.write('The random number is ' + randomNumber);
+
+
+// task 7 
+
+// var userInput = prompt('Please enter your weight')
+// var weight = parseFloat(userInput)
+// if(!isNaN (weight)){
+//     document.write('Your weight is ' + weight + 'Kgs')
+// }else{
+//     document.write('Invalid input. Please enter a valid wei')
+// }
+
+// // task 8 
+// var secretNumber  = Math.floor(Math.random()* 10 ) + 1;
+// var userInput = prompt('Guess the secret number (between 1 and 10)')
+// var userNumber = parseInt(userInput);
+// if(userNumber === secretNumber){
+//     document.write('Congratualation! You guessed the secret number correctly!')
+
+// }else{
+//     document.write('Oops! The secret number was' + secretNumber + 'better luck next time' )
+// }
